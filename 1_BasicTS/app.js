6938b0ccc0d1e907b19461041a3b76e9e5b7ce91
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-11-12 01:43:41 +0530

    Type Inference

--- 1_BasicTS/app.js
+++ 1_BasicTS/app.js
@@ -1,11 +1,14 @@
 function add(n1, n2) {
-    // JS throws error on runtime while TS throws error on DEV time
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('Format exception detected');
-    }
     return n1 + n2;
 }
-var n1 = "10";
+// Type Inference: In TypeScript, there are several places where type inference
+// is used to provide type information when there is no explicit type annotation
+// direct assignment makes variable to that specified type
+var n1 = 10;
 var n2 = 10.3;
+// Explicit type and
+// assigning wrong type
+var x;
+x = 0; // Type ERROR
 var addval = add(n1, n2);
 console.log(addval);
