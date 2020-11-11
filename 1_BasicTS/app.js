89763dc6c57935fe77f82f718b757296c9bcde8e
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-11-12 01:10:17 +0530

    useing TS Types on 1_BASICTS

---
+++ 1_BasicTS/app.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var n1 = "10";
+var n2 = 10.3;
+var addval = add(n1, n2);
+console.log(addval);
---
+++ 1_BasicTS/app.ts
@@ -0,0 +1,9 @@
+function add(n1: number, n2: number){
+      return n1+n2;
+}
+
+const n1 = "10";
+const n2 = 10.3;
+
+const addval = add(n1, n2);
+console.log(addval)
