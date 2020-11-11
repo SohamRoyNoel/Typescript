035d243a1763a1fcbb58baec29b8f539df5b310a
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-11-12 02:18:13 +0530

    4_Simple&Nested_ObjectType

--- 1_BasicTS/app.ts
+++ 1_BasicTS/app.ts
@@ -1,18 +1,35 @@
-function add(n1: number, n2: number){
-      return n1+n2;
+
+// CASE 1:- explicit Object type: as we are making it as Object
+// const user : Object = {
+//      name: "Soham",
+//      age: 23
+// }
+
+// CASE 2
+// Nested OBJECT
+interface hateList {
+      title: string;
+      description: string; 
 }
 
-// Type Inference: In TypeScript, there are several places where type inference
-// is used to provide type information when there is no explicit type annotation
+// Simple Object Type
+const user : {
+      name: String;
+      age: number;
+      loves: string[], // Array as OBJECT Type
+      hates: hateList[]       // Nested OBJECT
+} = {
+      name: "Soham",
+      age: 23,
+      loves: ['JS', 'TS', 'GQL'],
+      hates: [{
+            title:'SSMS',
+            description: 'I dont understand'
+      },
+      {
+            title: 'JSP',
+            description: 'This is dumbs haven'
+      }]
+}
 
-// direct assignment makes variable to that specified type
-let n1 = 10;
-const n2 = 10.3;
-
-// Explicit type and
-// assigning wrong type
-let x : string;
-x = 0; // Type ERROR
-
-const addval = add(n1, n2);
-console.log(addval)
+console.log(user.hates[1].title);
