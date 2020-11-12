3a8bab5e614a288458fe4214ef72aec2fd0d20c4
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-11-13 02:29:02 +0530

    Union

--- 1_BasicTS/app.js
+++ 1_BasicTS/app.js
@@ -1,25 +1,12 @@
-// default enum starts with 0
-var role;
-(function (role) {
-    role[role["DEV"] = 0] = "DEV";
-    role[role["SUPPORT"] = 1] = "SUPPORT";
-    role[role["QA"] = 2] = "QA";
-    role[role["PO"] = 3] = "PO";
-})(role || (role = {}));
-;
-// you can assign enum value to anything, it will be counted from there
-var role1;
-(function (role1) {
-    role1[role1["DEV"] = 6] = "DEV";
-    role1[role1["SUPPORT"] = 7] = "SUPPORT";
-    role1[role1["QA"] = 8] = "QA";
-    role1[role1["PO"] = 9] = "PO";
-})(role1 || (role1 = {}));
-var person = {
-    name: "Soham",
-    age: 23,
-    languages: ["TS", "JS", "React", "Node"],
-    efficiency: ['TS', 1],
-    role: role1.PO
-};
-console.log(person.role);
+function add(n1, n2) {
+    var res;
+    if (typeof n1 === 'number' && typeof n2 === 'number') {
+        res = n1 + n2;
+    }
+    else {
+        res = n1.toString() + n2.toString();
+    }
+    return res;
+}
+console.log(add(10, 20));
+console.log(add('10', '20'));
