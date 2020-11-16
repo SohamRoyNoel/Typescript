4139f3baf15886922b38d7a57941b1a503b8a099
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-11-17 01:33:42 +0530

    Function as types and callbacks

--- 1_BasicTS/app.js
+++ 1_BasicTS/app.js
@@ -1,14 +1,8 @@
 // In TS we dont need to specify return type explicitly, TS inference does it for us
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, cb) {
+    var x = n1 + n2;
+    cb(x);
 }
-// Void and Undefined type :: void means no return type
-function printer(num) {
-    console.log(num);
-}
-function printer1(num) {
-    console.log(num);
-    return;
-}
-var addVals = add;
-console.log(addVals(8, 8));
+console.log(add(8, 10, function (resp) {
+    console.log(resp);
+}));
