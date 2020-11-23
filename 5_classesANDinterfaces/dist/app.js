58e01f4837a4fa6379b0f9b39cdfe79e325126e8
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-11-24 01:17:47 +0530

    TS interface and getter setter

--- 5_classesANDinterfaces/dist/app.js
+++ 5_classesANDinterfaces/dist/app.js
@@ -15,9 +15,45 @@ class Department {
         console.log(`Dept : ${this.name} and ${this.i}`);
     }
 }
-const dept = new Department('Developer', 10);
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super('IT', id);
+        this.admins = admins;
+        this.lastAdmin = '';
+    }
+    get getLastAdmin() {
+        if (this.lastAdmin) {
+            return this.lastAdmin;
+        }
+        throw new Error('No Admin IS created');
+    }
+    set setLastAdmin(value) {
+        if (!value) {
+            throw new Error('Are you crazy! Giving me a shitty null');
+        }
+        this.setAdmins(value);
+    }
+    setEmployee(nm) {
+        if (nm === 'Soham') {
+            return;
+        }
+        this.employees.push(nm);
+    }
+    setAdmins(admin) {
+        this.admins.push(admin);
+        this.lastAdmin = admin;
+    }
+    getAdmins() {
+        console.log(`Admins are ${this.admins}`);
+    }
+}
+const dept = new ITDepartment(10, []);
+dept.getAdmins();
 console.log(dept);
 dept.department();
-dept.setEmployee('Anny');
-dept.setEmployee('Merry');
+dept.setEmployee('Soham');
+dept.setEmployee('Candy');
 dept.getEmployee();
+dept.setAdmins('Ivy');
+dept.setLastAdmin = 'Soham';
+console.log("VAL from getter setter " + dept.getLastAdmin);
