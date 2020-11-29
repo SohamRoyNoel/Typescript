a8149a2cd2727e99ae6980e51b9386208209eb40
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-11-30 01:51:42 +0530

    Advanced Types

---
+++ 6_AdvancedTStypes/src/app.ts
@@ -0,0 +1,26 @@
+const me = {
+  name: 'Soham Roy',
+  role: 'Developer',
+  efficient: {
+    fe : ['React', 'Flutter'],
+    be: ['express'],
+    db: ['mongo']
+  }
+}
+
+// Optional chaining is done with ?, that means if that property exists then find 
+// In case of API call
+// console.log(me.efficients?.fe);
+
+console.log(me.efficient.fe);
+
+////////// NULLISH COALESCING
+// const c = ''; // Returns a empty string
+const c = undefined;
+
+// NULLISH : ?? ====> It refers if c is NULL or UNDEFINED then only use the fall back value
+
+const s = c ?? 'NUll or Undefined found'
+
+
+console.log(s);
---
+++ 6_AdvancedTStypes/src/functionOverload.ts
@@ -0,0 +1,11 @@
+// type mix = string | number;
+
+// function add(a: number, b: number): number;
+// function add(a: string, b: string): string;
+// function add(a: mix, b: mix){
+//   if(typeof a === 'string' || typeof b === 'string')
+//     return a.toString()  +  b.toString() ;
+//   return a + b;
+// }
+
+// console.log("Ret : " + add('5', '10'));
